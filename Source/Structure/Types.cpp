1e6799b3496630b9b5070caa54c254ee574e0884
Gregory Loden <gregory.loden@gmail.com>
2017-09-03 22:35:55 -0400

    add all other (current) global types

--- Source/Structure/Types.cpp
+++ Source/Structure/Types.cpp
@@ -3,6 +3,12 @@
 PrefixTrie<char, CType*>* CType::globalTypes = []() -> PrefixTrie<char, CType*>* {
 	PrefixTrie<char, CType*>* val = new PrefixTrie<char, CType*>();
 	val->set("void", 4, new CVoid());
+	val->set("bool", 4, new CIntegerPrimitive(1));
+	val->set("byte", 4, new CIntegerPrimitive(8));
+	val->set("short", 5, new CIntegerPrimitive(16));
+	val->set("int", 3, new CIntegerPrimitive(32));
+	val->set("Function", 8, new CFunctionType());
+	val->set("String", 6, new CClass());
 	return val;
 }();
 CType::CType(onlyWhenTrackingIDs(char* pObjType COMMA))
@@ -13,3 +19,24 @@ CVoid::CVoid()
 : CType(onlyWhenTrackingIDs("VOIDTYP")) {
 }
 CVoid::~CVoid() {}
+CPrimitive::CPrimitive(onlyWhenTrackingIDs(char* pObjType COMMA) short pBitSize)
+: CType(onlyWhenTrackingIDs(pObjType COMMA))
+, bitSize(pBitSize) {
+}
+CPrimitive::~CPrimitive() {}
+CIntegerPrimitive::CIntegerPrimitive(int pByteSize)
+: CPrimitive(onlyWhenTrackingIDs("INTPMTP" COMMA) pByteSize) {
+}
+CIntegerPrimitive::~CIntegerPrimitive() {}
+CFloatingPointPrimitive::CFloatingPointPrimitive(int pByteSize)
+: CPrimitive(onlyWhenTrackingIDs("FLTPMTP" COMMA) pByteSize) {
+}
+CFloatingPointPrimitive::~CFloatingPointPrimitive() {}
+CFunctionType::CFunctionType()
+: CType(onlyWhenTrackingIDs("FUNCTYP")) {
+}
+CFunctionType::~CFunctionType() {}
+CClass::CClass()
+: CType(onlyWhenTrackingIDs("CLSSTYP")) {
+}
+CClass::~CClass() {}
--- Source/Structure/Types.h
+++ Source/Structure/Types.h
@@ -1,8 +1,9 @@
 template <class Key, class Value> class PrefixTrie;
 
 class CType onlyInDebug(: public ObjCounter) {
-public:
+protected:
 	CType(onlyWhenTrackingIDs(char* pObjType));
+public:
 	virtual ~CType();
 
 	static PrefixTrie<char, CType*>* globalTypes;
@@ -12,3 +13,48 @@ public:
 	CVoid();
 	virtual ~CVoid();
 };
+class CPrimitive: public CType {
+protected:
+	CPrimitive(onlyWhenTrackingIDs(char* pObjType COMMA) short pBitSize);
+public:
+	virtual ~CPrimitive();
+
+	short bitSize; //copper: readonly
+};
+class CIntegerPrimitive: public CPrimitive {
+public:
+	CIntegerPrimitive(int pByteSize);
+	virtual ~CIntegerPrimitive();
+};
+class CFloatingPointPrimitive: public CPrimitive {
+public:
+	CFloatingPointPrimitive(int pByteSize);
+	virtual ~CFloatingPointPrimitive();
+};
+class CFunctionType: public CType {
+public:
+	CFunctionType();
+	virtual ~CFunctionType();
+};
+class CClass: public CType {
+public:
+	CClass();
+	virtual ~CClass();
+};
+/*
+class CEnum: public CType {
+public:
+	CEnum();
+	virtual ~CEnum();
+};
+class CGenericPrimitive: public CPrimitive {
+public:
+	CGenericPrimitive();
+	virtual ~CGenericPrimitive();
+};
+class CGenericClass: public CClass {
+public:
+	CGenericClass();
+	virtual ~CGenericClass();
+};
+*/
