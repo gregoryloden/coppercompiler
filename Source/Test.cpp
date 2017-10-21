62faf8c3bb6bf593570a91645607f30fe49d7c4c
Gregory Loden <gregory.loden@gmail.com>
2017-10-20 20:24:47 -0400

    add more lex tests

--- Source/Test.cpp
+++ Source/Test.cpp
@@ -4,6 +4,15 @@
 	void Test::testFiles() {
 		testFile("Test/Step01_Lex_lex.cu", 0);
 		testFile("Test/Step01_Lex_badLex.cu", 10);
+		testFile("Test/Step01_Lex_blockCommentEOF.cu", 1);
+		testFile("Test/Step01_Lex_intBaseEOF.cu", 1);
+		testFile("Test/Step01_Lex_floatEOF.cu", 1);
+		testFile("Test/Step01_Lex_stringEOF.cu", 1);
+		testFile("Test/Step01_Lex_stringEscapeSequenceEOF.cu", 1);
+		testFile("Test/Step01_Lex_stringHexEscapeSequenceEOF.cu", 1);
+		testFile("Test/Step01_Lex_characterEOF.cu", 1);
+		testFile("Test/Step01_Lex_characterUnterminatedEOF.cu", 1);
+		testFile("Test/Step01_Lex_directiveEOF.cu", 1);
 	}
 	void Test::testFile(const char* fileName, int errorsExpected) {
 		Pliers* p = new Pliers(fileName, false, false);
